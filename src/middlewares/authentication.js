9d67182784bdc86c8110e2ebcea0a6cf307a6012
stormbless <75245920+stormbless@users.noreply.github.com>
2025-06-01 22:58:32 +1000

    Modified a few log statements

--- src/middlewares/authentication.js
+++ src/middlewares/authentication.js
@@ -5,28 +5,23 @@ const jwt = require('jsonwebtoken');
 
 async function authenticationMiddleware(req, res, next) {
   console.log("received request");
-  console.log(JSON.stringify(req.body));
   try {
     const sessionToken = req.headers.authorization;
-
 
     const { dat } = jwt.verify(
       sessionToken,
       secretsManager.get("CLIENT_SECRET")
     );
 
-    // console.log(`dat: ${JSON.stringify(dat)}`);
-
+    // are numbers (not strings)
     const accountId = dat.account_id;
     const userId = dat.user_id;
-
-    // console.log(`accountId: ${accountId} userId: ${userId}`);
-    // console.log(`accountId type: ${typeof accountId} userId type: ${typeof userId}`);
 
     req.session = { accountId, userId };
     next();    
     console.log("received valid token");
   } catch (err) {
+    console.log('received invalid or expired token');
     console.error(err);
     return res.status(500).send('not authenticated' );
   }
