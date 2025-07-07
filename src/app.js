24d1e504021125829ba076742314e8adaea80580
stormbless <75245920+stormbless@users.noreply.github.com>
2025-07-07 18:02:20 +1000

    CORS update
    -removed some console logs
    -made it so port is default set to 8080
    -replaced the cors allowed urls for new avq account

--- src/app.js
+++ src/app.js
@@ -1,33 +1,24 @@
 const { EnvironmentVariablesManager } = require('@mondaycom/apps-sdk');
 
-let envManager = new EnvironmentVariablesManager();// const jwt = require('jsonwebtoken');
+let envManager = new EnvironmentVariablesManager();
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes');
 const cors = require('cors');
 
-const port = envManager.get("PORT");
+const port = envManager.get("PORT") || 8080;
 const app = express();
 
-// possibly restrict to https://gro3demo.monday.com
 const corsOptions = {
   origin: [
-    'https://vbd2f1a6673a77bd8.cdn2.monday.app', // item view 
-    'https://23f4f27adb43e5ad.cdn2.monday.app', // account settings view
+    'https://v5e69c38b63261f9a.cdn2.monday.app', // item view 
+    'https://96360fdd98b1e756.cdn2.monday.app', // account settings view
   ],
   method: 'GET, POST'
 };
 // if (process.env.NODE_ENV === "test") {
 //   corsOptions.origin.push('http://localhost:8301');
 // }
-
-// const token = jwt.sign({
-//   dat: {
-//     account_id: "1",
-//     user_id: "1"
-//   }
-// }, process.env.CLIENT_SECRET, {expiresIn: 48 * 60 * 60 });
-// console.log(`start of token: ${token} end of token`);
 
 app.use(cors(corsOptions));
 
